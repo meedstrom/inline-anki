8932ef18eebecb298e6c5c1400d6155408796dc4
Martin Edström <meedstrom91@gmail.com>
2023-09-20 05:30:24 +0200

    Then there was light

---
+++ inline-anki-anki-editor-fork.el
@@ -0,0 +1,304 @@
+;;; inline-anki-anki-editor-fork.el --- Functions from anki-editor  -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2018 Lei Tan <louietanlei@gmail.com>
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+(require 'cl-lib)
+(require 'dash)
+(require 'json)
+(require 'org-element)
+(require 'ox)
+(require 'ox-html)
+(require 'request)
+
+(defcustom inline-anki-break-consecutive-braces-in-latex
+  nil
+  "If non-nil, consecutive `}' will be automatically separated by spaces to prevent early-closing of cloze.
+See https://apps.ankiweb.net/docs/manual.html#latex-conflicts.")
+
+(defcustom inline-anki-protected-tags
+  '("marked" "leech")
+  "A list of tags that won't be deleted from Anki even though they're absent in Org entries, such as special tags `marked', `leech'."
+  :type '(repeat string))
+
+(defcustom inline-anki-ignored-org-tags
+  (append org-export-select-tags org-export-exclude-tags)
+  "A list of Org tags that are ignored when constructing notes form entries."
+  :type '(repeat string))
+
+(defcustom inline-anki-anki-connect-listening-address
+  "127.0.0.1"
+  "The network address AnkiConnect is listening.")
+
+(defcustom inline-anki-anki-connect-listening-port
+  "8765"
+  "The port number AnkiConnect is listening.")
+
+(defcustom inline-anki-use-math-jax nil
+  "Use Anki's built in MathJax support instead of LaTeX.")
+
+;;; AnkiConnect
+
+(defun inline-anki--anki-connect-action (action &optional params version)
+  (let (a)
+    (when version
+      (push `(version . ,version) a))
+    (when params
+      (push `(params . ,params) a))
+    (push `(action . ,action) a)))
+
+(defun inline-anki--anki-connect-invoke-queue ()
+  (let (action-queue)
+    (lambda (&optional action params handler)
+      (if action
+          (push (cons (inline-anki--anki-connect-action action params) handler) action-queue)
+        (when action-queue
+          (apply #'inline-anki--anki-connect-invoke-multi (nreverse action-queue))
+          (setq action-queue nil))))))
+
+(defun inline-anki--anki-connect-invoke (action &optional params)
+  "Invoke AnkiConnect with ACTION and PARAMS."
+  (let ((request-body (json-encode (inline-anki--anki-connect-action action params 5)))
+        (request-backend 'curl)
+        (json-array-type 'list)
+        reply err)
+
+    (let ((response (request (format "http://%s:%s"
+                                     inline-anki-anki-connect-listening-address
+                                     inline-anki-anki-connect-listening-port)
+                             :type "POST"
+                             :parser 'json-read
+                             :data request-body
+                             :success (cl-function (lambda (&key data &allow-other-keys)
+                                                     (setq reply data)))
+                             :error (cl-function (lambda (&key _ &key error-thrown &allow-other-keys)
+                                                   (setq err (string-trim (cdr error-thrown)))))
+                             :sync t)))
+
+      ;; HACK: With sync set to t, `request' waits for curl process to
+      ;; exit, then response data becomes available, but callbacks
+      ;; might not be called right away but at a later time, that's
+      ;; why here we manually invoke callbacks to receive the result.
+      (unless (request-response-done-p response)
+        (request--curl-callback (get-buffer-process (request-response--buffer response)) "finished\n")))
+
+    (when err (error "Error communicating with AnkiConnect using cURL: %s" err))
+    (or reply (error "Got empty reply from AnkiConnect"))))
+
+(defmacro inline-anki--anki-connect-invoke-result (&rest args)
+  "Invoke AnkiConnect with ARGS, return the result from response or raise an error."
+  `(let-alist (inline-anki--anki-connect-invoke ,@args)
+     (when .error (error .error))
+     .result))
+
+(defun inline-anki--anki-connect-invoke-multi (&rest actions)
+  (-zip-with (lambda (result handler)
+               (when-let ((_ (listp result))
+                          (err (alist-get 'error result)))
+                 (error err))
+               (and handler (funcall handler result)))
+             (inline-anki--anki-connect-invoke-result
+              "multi" `((actions . ,(mapcar #'car actions))))
+             (mapcar #'cdr actions)))
+
+(defun inline-anki--anki-connect-map-note (note)
+  "Convert NOTE to the form that AnkiConnect accepts."
+  (let-alist note
+    (list (cons "id" .note-id)
+          (cons "deckName" .deck)
+          (cons "modelName" .note-type)
+          (cons "fields" .fields)
+          ;; Convert tags to a vector since empty list is identical to nil
+          ;; which will become None in Python, but AnkiConnect requires it
+          ;; to be type of list.
+          (cons "tags" (vconcat .tags)))))
+
+
+;;; Org Export Backend
+
+(defconst inline-anki--ox-anki-html-backend
+  (if inline-anki-use-math-jax
+      (org-export-create-backend
+       :parent 'html
+       :transcoders '((latex-fragment . inline-anki--ox-latex-for-mathjax)
+                      (latex-environment . inline-anki--ox-latex-for-mathjax)))
+    (org-export-create-backend
+     :parent 'html
+     :transcoders '((latex-fragment . inline-anki--ox-latex)
+                    (latex-environment . inline-anki--ox-latex)))))
+
+(defun inline-anki--translate-latex-delimiters (latex-code)
+  (catch 'done
+    (let ((delimiter-map (list (list (cons (format "^%s" (regexp-quote "$$")) "[$$]")
+                                     (cons (format "%s$" (regexp-quote "$$")) "[/$$]"))
+                               (list (cons (format "^%s" (regexp-quote "$")) "[$]")
+                                     (cons (format "%s$" (regexp-quote "$")) "[/$]"))
+                               (list (cons (format "^%s" (regexp-quote "\\(")) "[$]")
+                                     (cons (format "%s$" (regexp-quote "\\)")) "[/$]"))
+                               (list (cons (format "^%s" (regexp-quote "\\[")) "[$$]")
+                                     (cons (format "%s$" (regexp-quote "\\]")) "[/$$]"))))
+          (matched nil))
+      (save-match-data
+        (dolist (pair delimiter-map)
+          (dolist (delimiter pair)
+            (when (setq matched (string-match (car delimiter) latex-code))
+              (setq latex-code (replace-match (cdr delimiter) t t latex-code))))
+          (when matched (throw 'done latex-code)))))
+    latex-code))
+
+(defun inline-anki--translate-latex-delimiters-to-anki-mathjax-delimiters (latex-code)
+  (catch 'done
+    (let ((delimiter-map (list (list (cons (format "^%s" (regexp-quote "$$")) "\\[")
+                                     (cons (format "%s$" (regexp-quote "$$")) "\\]"))
+                               (list (cons (format "^%s" (regexp-quote "$")) "\\(")
+                                     (cons (format "%s$" (regexp-quote "$")) "\\)"))))
+          (matched nil))
+      (save-match-data
+        (dolist (pair delimiter-map)
+          (dolist (delimiter pair)
+            (when (setq matched (string-match (car delimiter) latex-code))
+              (setq latex-code (replace-match (cdr delimiter) t t latex-code))))
+          (when matched (throw 'done latex-code)))))
+    latex-code))
+
+(defun inline-anki--wrap-latex (content)
+  "Wrap CONTENT with Anki-style latex markers."
+  (format "<p><div>[latex]</div>%s<div>[/latex]</div></p>" content))
+
+(defun inline-anki--wrap-latex-for-mathjax (content)
+  "Wrap CONTENT for Anki's native MathJax support."
+  (format "<p>%s</p>" content))
+
+(defun inline-anki--wrap-div (content)
+  (format "<div>%s</div>" content))
+
+(defun inline-anki--ox-latex (latex _contents _info)
+  "Transcode LATEX from Org to HTML.
+CONTENTS is nil.  INFO is a plist holding contextual information."
+  (let ((code (org-remove-indentation (org-element-property :value latex))))
+    (setq code
+          (pcase (org-element-type latex)
+            ('latex-fragment (inline-anki--translate-latex-delimiters code))
+            ('latex-environment (inline-anki--wrap-latex
+                                 (mapconcat #'inline-anki--wrap-div
+                                            (split-string (org-html-encode-plain-text code) "\n")
+                                            "")))))
+
+    (if inline-anki-break-consecutive-braces-in-latex
+        (replace-regexp-in-string "}}" "} } " code)
+      code)))
+
+(defun inline-anki--ox-latex-for-mathjax (latex _contents _info)
+  "Transcode LATEX from Org to HTML.
+CONTENTS is nil.  INFO is a plist holding contextual information."
+  (let ((code (org-remove-indentation (org-element-property :value latex))))
+    (setq code
+          (pcase (org-element-type latex)
+            ('latex-fragment (inline-anki--translate-latex-delimiters-to-anki-mathjax-delimiters code))
+            ('latex-environment (inline-anki--wrap-latex-for-mathjax
+                                 (mapconcat #'inline-anki--wrap-div
+                                            (split-string (org-html-encode-plain-text code) "\n")
+                                            "")))))
+
+    (if inline-anki-break-consecutive-braces-in-latex
+        (replace-regexp-in-string "}}" "} } " code)
+      code)))
+
+
+;;; Core Functions
+
+(defun inline-anki--push-note (note)
+  "Request AnkiConnect for updating or creating NOTE."
+  (if (= (alist-get 'note-id note) -1)
+      (inline-anki--create-note note)
+    (inline-anki--update-note note)))
+
+(defun inline-anki--create-note (note)
+  "Request AnkiConnect for creating NOTE."
+  (let ((queue (inline-anki--anki-connect-invoke-queue)))
+    (funcall queue
+             'addNote
+             `((note . ,(inline-anki--anki-connect-map-note note)))
+             #'inline-anki--set-note-id)
+
+    (funcall queue)))
+
+(defun inline-anki--update-note (note)
+  "Request AnkiConnect for updating fields and tags of NOTE."
+
+  (let ((queue (inline-anki--anki-connect-invoke-queue)))
+    (funcall queue
+             'updateNoteFields
+             `((note . ,(inline-anki--anki-connect-map-note note))))
+
+    (funcall queue
+             'notesInfo
+             `((notes . (,(alist-get 'note-id note))))
+             (lambda (result)
+               ;; update tags
+               (let* ((existing-note (car result))
+                      (tags-to-add (-difference (-difference (alist-get 'tags note)
+                                                             (alist-get 'tags existing-note))
+                                                inline-anki-ignored-org-tags))
+                      (tags-to-remove (-difference (-difference (alist-get 'tags existing-note)
+                                                                (alist-get 'tags note))
+                                                   inline-anki-protected-tags))
+                      (tag-queue (inline-anki--anki-connect-invoke-queue)))
+
+                 (when tags-to-add
+                   (funcall tag-queue
+                            'addTags `((notes . (,(alist-get 'note-id note)))
+                                       (tags . ,(mapconcat #'identity tags-to-add " ")))))
+
+                 (when tags-to-remove
+                   (funcall tag-queue
+                            'removeTags `((notes . (,(alist-get 'note-id note)))
+                                          (tags . ,(mapconcat #'identity tags-to-remove " ")))))
+
+                 (funcall tag-queue))))
+
+    (funcall queue)))
+
+;;; Commands
+
+(defun inline-anki-anki-connect-upgrade ()
+  "Upgrade AnkiConnect to the latest version.
+
+This will display a confirmation dialog box in Anki asking if you
+want to continue.  The upgrading is done by downloading the latest
+code in the master branch of its Github repo.
+
+This is useful when new version of this package depends on the
+bugfixes or new features of AnkiConnect."
+  (interactive)
+  (when (yes-or-no-p "NOTE: This will download the latest codebase of AnkiConnect to your system, which is not guaranteed to be safe or stable. Generally, you don't need this command, this is useful only when new version of this package requires the updates of AnkiConnect that are not released yet. Do you still want to continue?")
+    (let ((result (inline-anki--anki-connect-invoke-result "upgrade")))
+      (when (and (booleanp result) result)
+        (message "AnkiConnect has been upgraded, you might have to restart Anki to make it in effect.")))))
+
+
+(provide 'inline-anki-anki-editor-fork)
+
+;;; inline-anki-anki-editor-fork.el ends here
---
+++ inline-anki.el
@@ -0,0 +1,191 @@
+;; inline-anki.el -- oneliner flashcards -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 Martin Edström <meedstrom91@gmail.com>
+;;
+;; Description: Oneliner flashcards
+;; Author: Martin Edström
+;; Version: 0.1.0-pre
+;; Package-Requires: ((emacs "28"))
+;; URL:
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; External requirements:
+;; - curl
+;; - Anki with AnkiConnect add-on
+
+;; TODO: make a command to auto-push an entire directory
+
+;;; Code:
+
+(defgroup inline-anki nil
+  "Customizations for inline-anki."
+  :group 'org)
+
+(require 'inline-anki-anki-editor-fork)
+
+(defcustom inline-anki-deck "Default"
+  "Name of deck to upload to."
+  :type 'string)
+
+(defcustom inline-anki-note-type "Cloze"
+  "Name of your note type."
+  :type 'string)
+
+(defcustom inline-anki-note-fields '("Text")
+  "Names of fields in your note type."
+  :type '(repeat string))
+
+(defcustom inline-anki-default-tags nil
+  "Tags to always include."
+  :type '(repeat string))
+
+(defcustom inline-anki-emphasis-type '(bold)
+  "Which emphasis type to parse as implicit clozes.
+Set this to '(bold), '(italic), or '(underline)."
+  :type 'sexp)
+
+(defcustom inline-anki-subscript-new-notes nil
+  "Whether to subscript instead of superscript."
+  :type 'boolean)
+
+(defun inline-anki-push-notes ()
+  (interactive)
+  (unless (eq major-mode 'org-mode)
+    (error "Not org-mode"))
+  (unless org-fontify-emphasized-text
+    (error "Inline-anki relies on `org-fontify-emphasized-text'"))
+  (inline-anki-map-note-things
+   (lambda ()
+     (message "Processing notes in buffer \"%s\", wait a moment..." (buffer-name))
+     (condition-case-unless-debug err
+         (inline-anki--push-note (inline-anki-note-at-point))
+       (error (message "Note at point %d failed: %s"
+                       (point)
+                       (error-message-string err)))))))
+
+;; TODO: send breadcrumbs as an extra field
+(defun inline-anki-note-at-point ()
+  "Construct an alist representing a note at point."
+  (let (;; Flag at start of list item? Exclude it from note text.
+        (begin (save-excursion
+                 ;; Org's magic puts us at at start of the list item after the
+                 ;; bullet point
+                 (goto-char (org-element-property
+                             :contents-begin (org-element-at-point)))
+                 (unless (search-forward "@anki" (+ 6 (point)) t)
+                   (re-search-forward (rx "@" (any "_^") "{" (= 13 digit) "}")
+                                      (+ 18 (point))
+                                      t))
+                 (point)))
+        ;; Flag at end of line?  Exclude it from note text.
+        (end (save-excursion
+               ;; Alas, (org-element-property :contents-end) jumps past all
+               ;; sub-items of a list-item.  So we just line-orient and hope
+               ;; that the user doesn't hard-wrap.
+               (goto-char (line-end-position))
+               (unless (re-search-backward (rx "@anki" (*? space) eol) begin t)
+                 (re-search-backward (rx (?? "@") (any "_^") "{" (*? nonl) "}" (*? space) eol)
+                                     begin
+                                     t))
+               (point))))
+
+    (list (cons 'deck inline-anki-deck)
+          (cons 'note-id (inline-anki-thing-id))
+          (cons 'note-type inline-anki-note-type)
+          (cons 'tags (append inline-anki-default-tags
+                              (mapcar #'substring-no-properties (org-get-tags))
+                              (list (format-time-string "from-emacs-%F"))))
+          ;; Drop text into the first field, however that's labeled
+          (cons 'fields (list (cons (car inline-anki-note-fields)
+                                    (inline-anki-convert-implicit-clozes
+                                     (buffer-substring begin end))))))))
+
+(defun inline-anki--set-note-id (id)
+  (unless id
+    (error "Note creation failed for unknown reason"))
+  (goto-char (line-beginning-position))
+  (let ((eol-flag (rx (any "_^") "{" (group "anki") "}" (*? space) eol)))
+    (cond
+     ((search-forward "@anki" (line-end-position) t)
+      (delete-char -4)
+      (insert "^{" (number-to-string id) "}"))
+
+     ((re-search-forward eol-flag (line-end-position) t)
+       (replace-match (number-to-string id) t t nil 1))
+
+     (t
+      (error "Inline-anki magic string not found on line")))))
+
+(defun inline-anki-thing-id ()
+  (save-excursion
+    (let ((beg (line-beginning-position))
+          (bound (progn
+                   (if (org-at-item-p)
+                       (org-end-of-item)
+                     (org-forward-paragraph))
+                   (point))))
+      (goto-char beg)
+      (if (re-search-forward (rx (any "_^") "{" (group (= 13 digit)) "}") bound t)
+          (string-to-number (match-string 1))
+        -1))))
+
+;; TOOD: detect if a line is a comment, and ignore
+;; TODO: detect if there is more than one flag on the same line (run all
+;; searches upfront, record the line numbers, then check for duplicates among
+;; the recorded line numbers)
+(defun inline-anki-map-note-things (func)
+  "Run FUNC with point on each flashcard in the buffer.
+Note: FUNC should not move point backwards, or you can get an
+infinite loop."
+  (let* ((ctr 0)
+         (list-bullet (rx (or (any "-+*") (seq (*? digit) (any ".)")))))
+         (card@item-start&has-id (rx bol (*? space) (regexp list-bullet) (+? space) "@" (any "_^") "{" (= 13 digit) "}"))
+         (card@item-start&new (rx bol (*? space) (regexp list-bullet) (+? space) "@anki"))
+         (card@eol&has-id (rx (? "@") (any "_^") "{" (= 13 digit) "}" (*? space) eol))
+         (card@eol&new (rx (or "@anki" "_{anki}" "^{anki}") (*? space) eol)))
+    (dolist (regexp (list card@item-start&has-id
+                          card@item-start&new
+                          card@eol&has-id
+                          card@eol&new))
+      (goto-char (point-min))
+      (while (re-search-forward regexp nil t)
+        (forward-char -1)
+        (cl-incf ctr)
+        (funcall func)))
+    ctr))
+
+(defun inline-anki-convert-implicit-clozes (text)
+  (with-temp-buffer
+    (insert text)
+    (goto-char (point-min))
+    (let ((n 0))
+      (while (setq prop (text-property-search-forward
+                         'face inline-anki-emphasis-type t))
+        (let ((num (number-to-string (cl-incf n))))
+          (goto-char (prop-match-beginning prop))
+          (delete-char -1)
+          (insert "{{c" num "::")
+          (goto-char (+ (prop-match-end prop) 4 (length num)))
+          (delete-char 1)
+          (insert "}}")))
+      (when (= n 0)
+        (error "No clozes in note: %s" text)))
+    (buffer-substring-no-properties (point-min) (point-max))))
+
+(provide 'inline-anki)
