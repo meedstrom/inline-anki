61b173658fc62a272367aa38f714c840edc7ae47
Martin Edström <meedstrom91@gmail.com>
2023-09-29 23:48:42 +0200

    Readme

--- inline-anki.el
+++ inline-anki.el
@@ -5,6 +5,7 @@
 ;; Description: One-liner flashcards
 ;; Author: Martin Edström
 ;; Version: 0.1.1-pre
+;; Created: 2023-09-19
 ;; Package-Requires: ((emacs "28") (asyncloop "0.3.2") (pcre2el "1.12") (request "0.3.0") (dash "2.12.0"))
 ;; URL: https://github.com/meedstrom/inline-anki
 
